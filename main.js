bfc2ffad55a35205c5a6351084afc235a3e1a7a6
Kris Iniguez <Kris.Iniguez.94@gmail.com>
2018-03-13 13:47:11 -0400

    Added a function in order to randomnly place the fruit which the snake will eat.

--- main.js
+++ main.js
@@ -3,12 +3,22 @@ window.onload = function() {
     ctx = canvas.getContext('2d');
     score = 0;
     level = 0;
+    direction = 0
+
+  var map = new Array(20);
+  for (var i = 0; i < map.length; i++) {
+    map[i] = new Array(20);
+  }
+
   canvas.width = 204;
   canvas.height = 224;
+
+
 
   var body = document.querySelector('body');
   body.appendChild(canvas);
 
+  map = generateFood(map);
   drawGame();
 
   function drawGame() {
@@ -22,8 +32,22 @@ window.onload = function() {
 
     ctx.strokeRect(2, 20, canvas.width - 4, canvas.height - 24);
 
-    ctx.font = '12px sans sariff';
-    ctx.fillText('Score: ' + score +'- Level: ' + level, 2, 12);
+    ctx.font = '14px sans-sariff';
+    ctx.fillText('Score: ' + score +' Level: ' + level, 2, 12);
+  }
+
+  function generateFood(map) {
+    var rndX = Math.round(Math.random() * 19)
+     rndY = Math.round(Math.random() * 19)
+    while (map[rndX][rndY] === 2) {
+      rndX = Math.round(Math.random() * 19);
+      rndY = Math.round(Math.random() * 19);
+       }
+
+       map[rndX][rndY] = 1;
+       return map;
+
+
   }
 
 };
